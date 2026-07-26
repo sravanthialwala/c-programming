4bea556ca6c27890cc0a4bf4dddb32880b280fa8
sravanthi alwala <alwalasravanthi294@gmail.com>
2026-07-26 14:11:35 +0530

    Add files via upload

---
+++ switchcase.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void main()
+	{
+		int num;
+		printf("enter a number:");
+		scanf("%d", &num);
+	switch(num){
+		case 10:
+			printf("good");
+			break;
+			case 20:
+				printf("very good");
+				break;
+				case 30:
+					printf("excellent");
+					default:
+						printf("invalid number");
+					
+	}
+}
